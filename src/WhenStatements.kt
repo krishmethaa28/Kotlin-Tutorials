23850d40d546119c0fba9f06d867500abf7fc4f5
krishmethaa28 <krishmekrish28@gmail.com>
2021-09-20 23:58:47 +0530

    If Else conditions

---
+++ src/WhenStatements.kt
@@ -0,0 +1,13 @@
+//When statements in Kotlin are like Switch statements in Java just without break
+
+fun main() {
+    val num1 = 3
+
+    when(num1){
+
+        1 -> println("It is a One")
+        2 -> println("It is a Two")
+        3 -> println("It is a Three")
+        else -> println("Please enter a default value")
+    }
+}
