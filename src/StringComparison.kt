66695c7a97d79897a2c934ea13fb3dbf85cabeb0
krishmethaa28 <krishmekrish28@gmail.com>
2021-09-20 23:16:50 +0530

    String comparison in kotlin

---
+++ src/StringComparison.kt
@@ -0,0 +1,12 @@
+
+fun main() {
+
+    var str1 : String = "Krishme"
+    var str2 : String = "Shadu"
+
+    if (str1==str2)  // (str1.equals(str2))
+        println("Both the names $str1 and $str2 are same")
+
+    else
+        println("Both the names $str1 and $str2 are different")
+}
