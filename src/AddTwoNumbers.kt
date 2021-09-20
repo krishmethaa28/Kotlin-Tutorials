e91dc1bf088679d52e0cc5f4d4937afb3e4287d3
krishmethaa28 <krishmekrish28@gmail.com>
2021-09-20 20:29:08 +0530

    If Else conditions

--- src/AddTwoNumbers.kt
+++ src/AddTwoNumbers.kt
@@ -1,3 +1,5 @@
+//operators work same as java, only the variable namespace differ { int in java}
+
 fun main() {
     val num1 : Int = 23
     val num2 : Int = 46
@@ -7,8 +9,8 @@ fun main() {
     val product = num1 * num2
     val ratio = num2 / num1
 
-    println("The sum of ${num1} and ${num2} is ${total}" )
-    println("The diff of ${num1} and ${num2} is ${diff}" )
-    println("The product of ${num1} and ${num2} is ${product}" )
-    println("The ratio of ${num1} and ${num2} is ${ratio}" )
+    println("The sum of $num1 and $num2 is $total" )
+    println("The diff of $num1 and $num2 is $diff" )
+    println("The product of $num1 and $num2 is $product" )
+    println("The ratio of $num1 and $num2 is $ratio" )
 }
---
+++ src/IfElseConditions.kt
@@ -0,0 +1,14 @@
+// Written as a ternary operator
+
+fun main() {
+    val num1  = 28
+    val num2  = 36
+
+    val greater = if(num2 > num1)
+        num2
+    else
+        num1
+
+    println("The greater num is $greater")
+
+}
