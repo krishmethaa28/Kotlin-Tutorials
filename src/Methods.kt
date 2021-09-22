62968e8322fd02d3d845fe1ed0d892ef0e8273ed
krishmethaa28 <krishmekrish28@gmail.com>
2021-09-22 23:31:07 +0530

    Methods to add & find Max

---
+++ src/Methods.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    println("Add(2,3) : " + add(2,3))
+    println("Add(7,2) : " + addNum(7,2))
+    println("Max(6,4) : " + max(6,4))
+    println("Max(2,3) : " + maxNum(2,3))
+}
+
+//add two numbers
+fun add(a : Int, b : Int) : Int{
+    return a + b
+}
+
+//add function in one line
+fun addNum(a : Int, b : Int) : Int = (a + b)
+
+//find max
+fun max(a : Int, b : Int) : Int{
+    return if (a>b)
+        a
+    else
+        b
+
+}
+
+//find max in one line
+fun maxNum(a: Int, b: Int) : Int = if (a>b) a else b
