6c2e87fb3028ba3628e009f7810ba7e9c49d3f8f
krishmethaa28 <krishmekrish28@gmail.com>
2021-09-24 22:51:31 +0530

    Secondary Constructor

--- src/Constructors.kt
+++ src/Constructors.kt
@@ -1,18 +1,32 @@
 
 //Primary Constructor
 class Human (var n : String){
-    var name : String = n
+    var name : String = ""
+
+//init block is used to do operations inside constructors
+    init {
+        name = n
+        println("The owner name is assigned as : $name.")
+
+    }
 
     fun think(){
-        println("$name can think well")
+        println("$name has a pet.")
     }
 }
 
-class Animal{
-    var limbs : Int = 0
+class Animal (var noOfLimbs : Int) {
+
+    var limbs : Int = noOfLimbs
+    var eyes : Int = 0
+
+ //Secondary Constructor
+    constructor(limbs : Int, eyes : Int) : this(limbs) {
+        this.eyes = eyes
+    }
 
     fun walk(){
-        println("I have 04 limbs")
+        println("The pet has $limbs limbs and $eyes eyes.")
     }
 }
 
@@ -23,7 +37,7 @@ fun main() {
     human.think()
 
 //Class Animal
-    var animal = Animal()
+    var animal = Animal(4,2)
     animal.walk()
 
 }
