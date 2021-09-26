4e1e25774081f3e9e785cab4ac341b762cae6c00
krishmethaa28 <krishmekrish28@gmail.com>
2021-09-26 23:43:53 +0530

    Operations with List

---
+++ src/List.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var values = mutableListOf<Int>(10,30,40,50,60)
+    values.add(70)
+    values.add(1,20)
+
+    for (i in values)
+        print("$i ")
+
+    println()
+    println("Last : ${values.last()}")
+    println("Size :  ${values.size}")
+}
