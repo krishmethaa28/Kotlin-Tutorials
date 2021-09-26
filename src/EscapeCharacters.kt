8c9a66caf29397bd5f522ce8a81f18196c4e1ede
krishmethaa28 <krishmekrish28@gmail.com>
2021-09-26 19:47:00 +0530

    Using backtick to escape characters & use them in method name & vice versa

---
+++ src/EscapeCharacters.kt
@@ -0,0 +1,8 @@
+fun `in`(){
+    println("Purpose : Using backtick to escape characters & use them in method name & vice versa ")
+}
+
+fun main() {
+    `in`()  //use backtick everywhere around keywords
+
+}
